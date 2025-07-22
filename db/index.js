74291223079bb4622af56a3042c8194385a1e5e8
Bartosz Kowynia <164752835+Bartosz-Kow@users.noreply.github.com>
2025-07-22 12:11:18 +0200

    Add Messages Table

--- db/index.js
+++ db/index.js
@@ -1,4 +1,6 @@
 const Database = require("better-sqlite3");
 const db = new Database("database.db");
 require("./users")(db);
+require("./admins")(db);
+require("./messages")(db);
 module.exports = db;
